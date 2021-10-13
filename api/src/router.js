e343f7e0825e6280ccc94f59652aa86b7577137b
khemiri <khemiri_abdelmoneem@hotmail.com>
2021-10-13 19:00:09 +0000

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
+const imageProcessor = require("./imageProcessor");
 
 const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
 const storage = multer.diskStorage({
@@ -27,7 +28,7 @@ function fileFilter(request, file, callback) {
 router.get("photo-viewer", (request, response) => {
   response.sendFile(photoPath);
 });
-router.post("/upload", upload.single("photo"), (request, response) => {
+router.post("/upload", upload.single("photo"), async (request, response) => {
   if (request.fileValidationError) {
     response.status(400).json({
       error: request.fileValidationError,
@@ -37,5 +38,8 @@ router.post("/upload", upload.single("photo"), (request, response) => {
       success: true,
     });
   }
+  try {
+    await imageProcessor(request.file.filename);
+  } catch {}
 });
 module.exports = router;
