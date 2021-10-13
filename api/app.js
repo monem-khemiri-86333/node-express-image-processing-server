588c039f4bd875ae9f3b11501013588e37f2520a
khemiri <khemiri_abdelmoneem@hotmail.com>
2021-10-13 12:47:10 +0000

    Module completed

--- api/app.js
+++ api/app.js
@@ -4,9 +4,8 @@ const router = require("./src/router");
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, "../client/index.html");
-app.use(express.static(path.resolve(__dirname, "uploads")));
 app.use("/", router);
-
+app.use(express.static(path.resolve(__dirname, "uploads")));
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
 });
